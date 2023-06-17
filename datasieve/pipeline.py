chore: suppress variance threshold. fix: ensure features_in is separate from final feature list

datasieve/pipeline.py
@@ -152,21 +152,22 @@ class Pipeline:
         if isinstance(X, pd.DataFrame) and fit:
             self.pandas_types = True
             self.feature_list = X.columns
+            self.features_in = X.columns
             if y is not None:
                 self.label_list = y.columns
         elif fit:
             self.pandas_types = False
-            self.feature_list = list(np.arange(0, X.shape[1]))
+            self.features_in = list(np.arange(0, X.shape[1]))
             if y is not None:
                 if len(y.shape) > 1:
                     self.label_list = list(np.arange(0, y.shape[1]))
                 else:
                     self.label_list = [0]
         elif isinstance(X, pd.DataFrame) and not fit:
-            if list(X.columns) != list(self.feature_list):
-                raise Exception(f"Pipeline expected {self.feature_list} but got {X.columns}.")
+            if list(X.columns) != list(self.features_in):
+                raise Exception(f"Pipeline expected {self.features_in} but got {X.columns}.")
         elif not isinstance(X, pd.DataFrame) and not fit and self.pandas_types:
-            X = pd.DataFrame(X, columns=self.feature_list)
+            X = pd.DataFrame(X, columns=self.features_in)
 
         if self.pandas_types:
             try:

datasieve/transforms/variance_threshold.py
@@ -23,9 +23,11 @@ class VarianceThreshold(BaseTransform):
         self.mask = self._skl.get_support()
         if feature_list is not None:
             self.feature_list = np.array(feature_list)[self.mask]
-            logger.info("Variance will remove features "
-                        f"{len(feature_list) - len(self.feature_list)} "
-                        f"on transform. {np.array(feature_list)[~self.mask]}")
+            if len(feature_list) - len(self.feature_list) > 0:
+                logger.info("VarianceThreshold will remove "
+                            f"{len(feature_list) - len(self.feature_list)} "
+                            "features from the dataset."
+                            f"on transform. {np.array(feature_list)[~self.mask]}")
         else:
             self.feature_list = None
 